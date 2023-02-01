03ecd0f7fad20f802e028e04234b5a68ad7ec7bd
Harshitha SV <solaiharshitha0@gmail.com>
2023-02-01 23:56:46 +0100

    instamart

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,7 +1,13 @@
+import instamart from '../../assets/images/instamart.png';
 
 const Instamart = () => {
   return (
-    <div className="container">Instamart</div>
+    <div className="container">
+      <div className="flex flex-col justify-center items-center gap-10 p-5">
+        <span className='text-blue-dark font-bold text-4xl'>Coming Soon ...</span>
+        <img className="w-[300px]" alt="instamert" src= {instamart} />
+      </div>
+    </div>
   )
 }
 
